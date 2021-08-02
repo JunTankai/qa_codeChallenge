8687c7b3a5f03dee7528c314a37ddcf20188d02e
Garrett Takashi Ahn <garrettahn@Garretts-MacBook-Pro.local>
2021-08-02 15:26:55 -0700

    getting my project ready for submission

--- src/__tests__/employeeManager.test.ts
+++ src/__tests__/employeeManager.test.ts
@@ -39,4 +39,47 @@ describe("Employee Manager", () => {
       title: "Grand Poobah",
     });
   });
+describe("Challenge 3", () => {
+  it("1. Add one more Employee", async () => {
+    await em.addEmployee();
+    await em.selectEmployeeByName("New Employee");
+    await em.editEmployee({
+      name: "Tony Stark",
+      phone: "1010101010",
+      title: "Iron Man",
+    });
+    await em.saveChanges();
+    await em.selectEmployeeByName("Dollie Berry");
+    await em.selectEmployeeByName("Tony Stark");
+    let employee = await em.getEmployeeInfo();
+    expect(employee.name).toEqual("Tony Stark");
+    expect(employee.phone).toEqual("1010101010");
+    expect(employee.title).toEqual("Iron Man");
+  });
+  it("2. Cancelling an Edit", async () => {
+    await em.selectEmployeeByName("Bernice Ortiz");
+    await em.editEmployee({ title: "Toilet Scrubber" });
+    await em.cancelChanges();
+    let employee = await em.getEmployeeInfo();
+    expect(employee).toEqual({
+      id: 1,
+      name: "Bernice Ortiz",
+      phone: "4824931093",
+      title: "CEO",
+    });
+  });
+    it("3.) Editing and then navigating away without saving does not save changes", async () => {
+      await em.selectEmployeeByName("Bernice Ortiz");
+      await em.editEmployee({ title: "President of USA" });
+      await em.selectEmployeeByName("Phillip Weaver");
+      await em.selectEmployeeByName("Bernice Ortiz");
+      let employee = await em.getEmployeeInfo();
+      expect(employee).toEqual({
+        id: 1,
+        name: "Bernice Ortiz",
+        phone: "4824931093",
+        title: "CEO",
+      });
+    });
+  });
 });
